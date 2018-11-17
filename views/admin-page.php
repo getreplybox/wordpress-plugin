014dfbe305aaffadb512d23176c8d2b5b716b123
Ashley Rich <hello@ashleyrich.com>
2018-11-17 14:28:16 +0000

    Add input descriptions

--- views/admin-page.php
+++ views/admin-page.php
@@ -3,7 +3,7 @@
 } ?>
 
 <div class="wrap">
-    <h1><?php _e('ReplyBox', 'replybox'); ?></h1>
+	<h1><?php _e('ReplyBox', 'replybox'); ?></h1>
 
 	<form method="POST" action="<?php echo esc_html(admin_url('admin-post.php')); ?>">
 		<input type="hidden" name="action" value="replybox_settings">
@@ -15,7 +15,10 @@
     					<label for="site_id"><?php _e('Site ID', 'replybox'); ?></label>
     				</th>
     				<td>
-    					<input type="text" name="site_id" id="site_id" value="<?php echo $this->get_option('site_id'); ?>" class="regular-text">
+						<input type="text" name="site_id" id="site_id" value="<?php echo $this->get_option('site_id'); ?>" class="regular-text">
+						<p class="description">
+							<?php printf(__('Your site ID comes from %s.', 'replybox'), '<a href="https://getreplybox.com" target="_blank">ReplyBox</a>'); ?>
+						</p>
     				</td>
     			</tr>
     			<tr>
@@ -23,7 +26,10 @@
     					<label for="secure_token"><?php _e('Secure Token', 'replybox'); ?></label>
     				</th>
     				<td>
-    					<input type="text" name="secure_token" id="secure_token" value="<?php echo $this->get_option('secure_token'); ?>" class="regular-text" readonly>
+						<input type="text" name="secure_token" id="secure_token" value="<?php echo $this->get_option('secure_token'); ?>" class="regular-text" readonly>
+						<p class="description">
+							<?php printf(__('Enter your secure token under <strong>Site > Embed</strong> in %s. This will allow ReplyBox to sync comments with WordPress.', 'replybox'), '<a href="https://getreplybox.com" target="_blank">ReplyBox</a>'); ?>
+						</p>
     				</td>
     			</tr>
     		</tbody>
